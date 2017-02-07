3b45ad7872b58048c8068009733ee3b7f1b5350b
Chris Capistran <capistranc@gmail.com>
2017-02-07 14:58:05 -0800

    lifting calculator completed

---
+++ src/Components/ExerciseComponent.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chris on 1/1/17.
+ */
+
+import React, { Component } from 'react';
+
+class ExerciseComponent extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default ExerciseComponent;
---
+++ src/Components/OneRepMaxCalculator.js
@@ -0,0 +1,100 @@
+/**
+ * Created by chris on 2/7/17.
+ */
+import React, { Component } from 'react';
+
+
+const BryzckiMax = (weight, reps) => { //Conservative
+    weight = parseInt(weight,10);
+    reps = parseInt(reps,10);
+    var max = weight / (1.0278 - (0.0278*reps))
+
+    return parseInt(max, 10);
+};
+
+const LanderMax = (weight, reps) => { //MiddleGround
+    weight = parseInt(weight, 10);
+    reps = parseInt(reps, 10);
+
+    var max = weight / (1.013 - (0.267123 * reps));
+    return parseInt(max, 10);
+}
+
+
+const EpleyMax= (weight, reps) => { //Ambitious
+    weight = parseInt(weight, 10);
+    reps = parseInt(reps, 10);
+    var max = weight*reps*.033 + weight;
+
+    return parseInt(max, 10);
+};
+
+
+class OneRepMaxCalculator extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleReset= this.handleReset.bind(this);
+
+        this.state = {
+            weight: 0,
+            reps: 0,
+            orm: 0,
+        };
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const max = BryzckiMax(this.state.weight, this.state.reps)
+        this.setState({orm: max});
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const name = target.name;
+
+        this.setState({[name]: target.value})
+    }
+
+    handleReset(e) {
+        e.preventDefault();
+        this.setState({weight: 0, reps: 0, orm: 0})
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Weight Lifted  <input type='number' min='1' max='9999' name='weight' onChange={this.handleChange} value={this.state.weight}/></label>
+                    <label title='Enter up to 20 reps'>  Reps Performed <input type='number' min='1' max='20' name='reps' onChange={this.handleChange} value={this.state.reps}/> </label>
+                    <button  >Calculate </button> <button onClick={this.handleReset}> Reset </button>
+
+
+
+
+                </form> <br />
+
+                <div>
+                    <h3>One Rep Max: <input min='0' max='9999' type='number' value={parseInt(this.state.orm, 10)} /></h3> <br/>
+
+                    55% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.50, 10)} />
+                    60% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.55, 10)} />
+                    50% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.60, 10)} />
+                    65% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.65, 10)} />
+                    70% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.70, 10)} />
+                    75% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.75, 10)} />
+                    80% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.80, 10)} />
+                    85% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.85, 10)} />
+                    90% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.90, 10)} />
+                    95% <input min='0' max='9999' type='number' value={parseInt(this.state.orm*.95, 10)} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default OneRepMaxCalculator;
+
+
+//could be 2400-3200 calories depending on size of sandwich, good macro nutrient spread,
+//
